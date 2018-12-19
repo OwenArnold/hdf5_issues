165dbc3712f9bf53e7ee9f170041a7ccb7fe2e93
Owen Arnold <owen.arnold@stfc.ac.uk>
2018-12-19 11:19:45 +0000

    fully working example

--- README.md
+++ README.md
@@ -15,4 +15,21 @@ Running `cmake --build` (or say make directly) will produce an executable called
 
 The executable `hdf5_issues` will attempt to use `H5::H5Cpp` to read the file and test features.
 
+## Options
+
+To illustrate issues. CMake has a `BACKWARD_COMPAT_FILE` flag, which defaults to `OFF`. If this is set to `ON`
+then the test file will be made with a layout comaptible for reading via version 1.8 of HDF5.
+
+## Notes
+
+Note that the HDF5 find module looks for a CMake `HDF5_ROOT` variable. Building newer versions of HDF5, and 
+setting this will allow you to switch HDF5 versions (if different from system). You can optionally provide this
+The source for hdf5 is [here](https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git), and can be generated/built
+with CMake. I had to use CPack to create a "complete" directory (header, libraries, etc) that the HDF5 find
+module coud process. I simply packaged with cpack then `tar -xvzf` the tarball in place. This could then be 
+used as the `HDF5_ROOT` as described above. 
+
+HDF5 now comes with a set of [tools](https://support.hdfgroup.org/HDF5/doc/RM/Tools.html). These are also 
+built by default if you build from source. Note that [h5format_compat](https://support.hdfgroup.org/HDF5/doc/RM/Tools.html#Tools-FormatConvert)
+can be used to convert datasets from newer HDF5 layout to older ones. For example `h5format_convert new_file.hdf5 -d local_name`
 
--- make_file.py
+++ make_file.py
@@ -3,6 +3,8 @@ import os
 import h5py
 import os
 import argparse
+import re
+import semantic_version
 
 parser = argparse.ArgumentParser(description='Script to create test hdf5 file')
 
@@ -10,15 +12,27 @@ positional_args = parser.add_argument_group('Positional arguments')
 positional_args.add_argument(
         'OutFilePath', nargs='?',
         help='The full file directory to write to')
-
+optional_args = parser.add_argument_group('Optional arguments')
+optional_args.add_argument('-e', '--earliest', action='store_true', default=False,
+                               help='Forces h5py to write as backward compatible version')
 arguments = parser.parse_args()
 if not arguments.OutFilePath:
     raise ValueError("No directory specified")
 if sys.version_info[0] < 3:
     raise Exception("Must be using Python 3")
+
+# Check h5py is using 1.10.0 or greater
+h5py_info = h5py.version.info
+match = re.search("HDF5\s*(\d+\.\d+\.\d+)", h5py_info)
+lib_version = match.group(1)
+if semantic_version.Version(lib_version) < semantic_version.Version('1.10.0'):
+    raise Exception('h5py using old HDF5 version ', lib_version)
+
 # Make test file in parent directory
 parent_dir =arguments.OutFilePath
-f = h5py.File(os.path.join(parent_dir, 'test_file.hdf5'), 'w')
+# libver `latest` means 1.10.1 writing
+write_ver = 'earliest' if arguments.earliest else 'latest'
+f = h5py.File(os.path.join(parent_dir, 'test_file.hdf5'), 'w', libver=write_ver)
 # type variable length string
 dt = h5py.special_dtype(vlen=str)     
 dset = f.create_dataset("dataset", (1,), dtype=dt, data=u"this_is_a_utf8_var_length_string")
